3eff3d78fd0dfb7cc4bb8924d35a846a0a8c38f4
V1tor100c <vitor.estevao.paraguassu@gmail.com>
2026-06-21 16:55:39 -0300

    feat: Maquina de estado do loop0. NÃO TESTADOgit add .!

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,13 @@ const int pino_echo = 2;
 UltraSonicDistanceSensor distanceSensor(pino_trigger, pino_echo);
 
 const int iri = 14;
+bool estadoIR = false;
 
 const int bracoI[] = {30, 31, 32, 33};
+Servo braco11, braco12, braco13, braco14;
+Servo braco21, braco22, braco23, braco24;
 
+const int botaoIniciar = 13;
 const int botaoEmergencia = 12;
 
 int medidaSemCaixa = 15;
@@ -30,8 +34,23 @@ bool viACaixa = false;
 bool bracoOperando = false;
 bool caixaNaEsteira = false;
 
-bool estadoEmergencia = false;
+volatile bool estadoEmergencia = false;
 
+char tamanhoCaixaMedida = 'X';
+
+// Definição dos Estados do Sistema (Máquina de Estados)
+enum EstadosSistema {
+  AGUARDANDO_START,
+  MANIPULADOR1_PEGA_CAIXA,
+  ESTEIRA_TRANSPORTANDO,
+  AGUARDANDO_FIM_ESTEIRA,
+  MANIPULADOR2_SEPARA_CAIXA,
+  RESETANDO_MAQUINA,
+  EM_EMERGENCIA
+};
+EstadosSistema estadoAtual = AGUARDANDO_START;
+
+// Prototipação das funções
 void loop0(void *parament);
 void loop1(void *parament);
 
@@ -45,9 +64,15 @@ void sensorDeCaixa();
 
 void pegarCaixa();
 
-void emergencia();
+void colocarCaixaP();
+void colocarCaixaM();
+void colocarCaixaG();
+
+void IRAM_ATTR emergencia();
 
 void zeraTudo();
+
+
 
 void setup()
 {
@@ -82,10 +107,17 @@ void setup()
 
   pinMode(iri, INPUT);
 
-  for(int i = 0; i < 4; i++){
-    pinMode(bracoI[i], OUTPUT);
-  }
+  braco11.attach(30, 500, 2400);
+  braco12.attach(31, 500, 2400);
+  braco13.attach(32, 500, 2400);
+  braco14.attach(33, 500, 2400);
 
+  braco21.attach(34, 500, 2400);
+  braco22.attach(35, 500, 2400);
+  braco23.attach(36, 500, 2400);
+  braco24.attach(37, 500, 2400);
+
+  pinMode(botaoIniciar, INPUT);
   pinMode(botaoEmergencia, INPUT);
 }
 
@@ -103,28 +135,82 @@ void loop()
 ● Sincronização temporal do sistema.*/
 
 void loop0(void *parameter){
-  Serial.println("Task0");
-  while (true){
-    if (digitalRead(estadoEmergencia) == HIGH){
-      emergencia();
-      estadoEmergencia = true;
-      while (botaoEmergencia == true){
-        if (digitalRead(estadoEmergencia) == HIGH){
-          estadoEmergencia = false;
+  while (true) {
+    
+    // Se a interrupção detectou emergência, força o estado de erro
+    if (estadoEmergencia) {
+      estadoAtual = EM_EMERGENCIA;
+    }
+
+    switch (estadoAtual) {
+      
+      case AGUARDANDO_START:
+        if (digitalRead(botaoIniciar) == HIGH){
+          estadoAtual = MANIPULADOR1_PEGA_CAIXA;
+          while(digitalRead(botaoIniciar) == HIGH) { delay(10); }
         }
-      }
-    } 
-    if (leituraIR() == true)
-    {
-      desligar_esteira();
+
+      case MANIPULADOR1_PEGA_CAIXA:
+        Serial.println("[Status] Manipulador 1 Movendo...");
+        pegarCaixa();
+        estadoAtual = ESTEIRA_TRANSPORTANDO;
+        break;
+
+      case ESTEIRA_TRANSPORTANDO:
+        Serial.println("[Status] Ligando Esteira e Medindo Caixa...");
+        ligar_esteira();
+        // Faz a leitura física do ultrassônico e classifica
+        sensorDeCaixa(); 
+        if(tamanhoCaixaMedida != 'X'){
+          estadoAtual = AGUARDANDO_FIM_ESTEIRA;
+        }
+        break;
+
+      case AGUARDANDO_FIM_ESTEIRA:
+        // Continua com a esteira ligada até a caixa bater no segundo sensor
+        estadoIR = leituraIR();
+        if (estadoIR == LOW) {
+          desligar_esteira();
+          estadoAtual = MANIPULADOR2_SEPARA_CAIXA;
+        }
+        break;
+
+      case MANIPULADOR2_SEPARA_CAIXA:
+        Serial.print("[Status] Separando caixa do tipo: ");
+        Serial.println(tamanhoCaixaMedida);
+        
+        if(leituraIR() == true){
+          if(tamanhoCaixaMedida = 'P'){
+            colocarCaixaP();
+          }
+          else if(tamanhoCaixaMedida = 'M'){
+            colocarCaixaM();
+          }
+          else{
+            colocarCaixaG();
+          }
+          estadoAtual = RESETANDO_MAQUINA;
+        }
+        break;
+
+      case RESETANDO_MAQUINA:
+        zeraTudo();
+        estadoAtual = AGUARDANDO_START;
+        break;
+
+      case EM_EMERGENCIA:
+        desligar_esteira();
+        Serial.println("!!! SISTEMA EM PARADA DE EMERGENCIA !!!");
+        // Só sai do loop de emergência se o botão for destravado/solto
+        if (digitalRead(botaoEmergencia) == HIGH) { 
+          estadoEmergencia = false;
+          estadoAtual = AGUARDANDO_START;
+          Serial.println("-> Sistema Reiniciado com Sucesso.");
+        }
+        break;
     }
-    else
-    {
-      ligar_esteira();
-    }
-    sensorDeCaixa();
-    zeraTudo();
-    vTaskDelay(pdMS_TO_TICKS(10));
+
+    vTaskDelay(pdMS_TO_TICKS(100)); // Delay do FreeRTOS para não travar o processador
   }
 }
 
@@ -187,14 +273,17 @@ void sensorDeCaixa()
     if (tamanho < medidaCaixaP)
     {
       quantidadeCaixasP++;
+      tamanhoCaixaMedida = 'P';
     }
     else if (tamanho < medidaCaixaM)
     {
       quantidadeCaixasM++;
+      tamanhoCaixaMedida = 'M';
     }
     else if (tamanho < medidaCaixaG)
     {
       quantidadeCaixasG++;
+      tamanhoCaixaMedida = 'G';
     }
     else
     {
@@ -203,17 +292,57 @@ void sensorDeCaixa()
 }
 
 void pegarCaixa(){
-
+  braco11.write(45);
+  vTaskDelay(pdMS_TO_TICKS(30));
+  braco12.write(45);
+  braco13.write(45);
+  vTaskDelay(pdMS_TO_TICKS(30));
+  braco14.write(45);
 }
 
-void emergencia()
-{
+void colocarCaixaP(){
+  braco21.write(45);
+  vTaskDelay(pdMS_TO_TICKS(30));
+  braco22.write(45);
+  braco23.write(45);
+  vTaskDelay(pdMS_TO_TICKS(30));
+  braco24.write(15);
+}
+void colocarCaixaM(){
+  braco21.write(45);
+  vTaskDelay(pdMS_TO_TICKS(30));
+  braco22.write(45);
+  braco23.write(45);
+  vTaskDelay(pdMS_TO_TICKS(30));
+  braco24.write(45);
+}
+void colocarCaixaG(){
+  braco21.write(45);
+  vTaskDelay(pdMS_TO_TICKS(30));
+  braco22.write(45);
+  braco23.write(45);
+  vTaskDelay(pdMS_TO_TICKS(30));
+  braco24.write(75);
+}
+
+void emergencia(){
   desligar_esteira();
 }
 
-void zeraTudo()
-{
+void zeraTudo(){
   bracoOperando = false;
   caixaNaEsteira = false;
   viACaixa = false;
+
+  braco11.write(0);
+  braco12.write(0);
+  braco13.write(0);
+  braco14.write(0);
+
+  braco21.write(0);
+  braco22.write(0);
+  braco23.write(0);
+  braco24.write(0);
+
+  tamanhoCaixaMedida = 'X';
 }
