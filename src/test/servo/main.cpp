e3f69d0bba9efa146ff8c33c54a4ed5fde613f70
V1tor100c <vitor.estevao.paraguassu@gmail.com>
2026-07-04 22:41:56 -0300

    feat: PEGA A CAIXA COM VONTADE rsrsrsrs, e coloca na esteira

--- src/test/servo/main.cpp
+++ src/test/servo/main.cpp
@@ -95,25 +95,10 @@ uint8_t ServoEsquerdo = 1;
 uint8_t ServoBase = 2;
 
 uint8_t ultAngGarra = 180;
-uint8_t ultAngDireito = 50;
-uint8_t ultAngEsquerda = 50;
+uint8_t ultAngDireito = 180;
+uint8_t ultAngEsquerda = 90;
 uint8_t ultAngBase = 100;
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Iniciando controle de servos com PCA9685 e ESP32!");
-
-  // Inicia a comunicação com o módulo
-  pwm.begin();
-  
-  // Frequência do oscilador interno do PCA9685 (geralmente 27MHz, ajusta a precisão)
-  pwm.setOscillatorFrequency(27000000);
-  
-  // Servos analógicos padrão operam a uma frequência de 50 Hz
-  pwm.setPWMFreq(50);
-
-  delay(10);
-}
 
 // Função auxiliar para converter o ângulo (0 a 180) para o valor de pulso PWM
 int converterAnguloParaPulso(int angulo) {
@@ -144,16 +129,16 @@ void moveServoG(int ang){
 }
 
 void moveServoD(int ang){
-  if(ang > ultAngGarra){
-    for(int angulo = ultAngGarra; angulo <= ang; angulo++) {
+  if(ang > ultAngDireito){
+    for(int angulo = ultAngDireito; angulo <= ang; angulo++) {
       pwm.setPWM(ServoDireito, 0, converterAnguloParaPulso(angulo));
       Serial.println(angulo);
       delay(20);
       ultAngDireito = angulo;
     }
   }
-  else if(ang < ultAngGarra){
-    for(int angulo = ultAngGarra; angulo >= ang; angulo--) {
+  else if(ang < ultAngDireito){
+    for(int angulo = ultAngDireito; angulo >= ang; angulo--) {
       pwm.setPWM(ServoDireito, 0, converterAnguloParaPulso(angulo));
       Serial.println(angulo);
       delay(20);
@@ -166,6 +151,11 @@ void moveServoD(int ang){
 }
 
 void moveServoE(int ang){
+  // Serial.println("ServoEsquerdo");
+  // Serial.print("ANG: ");
+  // Serial.print(ang);
+  // Serial.print(", ULT ANG: ");
+  // Serial.println(ultAngEsquerda);
   if(ang > ultAngEsquerda){
     for(int angulo = ultAngEsquerda; angulo <= ang; angulo++) {
       pwm.setPWM(ServoEsquerdo, 0, converterAnguloParaPulso(angulo));
@@ -175,7 +165,7 @@ void moveServoE(int ang){
     }
   }
   else if(ang < ultAngEsquerda){
-    for(int angulo = ServoEsquerdo; angulo >= ang; angulo--) {
+    for(int angulo = ultAngEsquerda; angulo >= ang; angulo--) {
       pwm.setPWM(ServoEsquerdo, 0, converterAnguloParaPulso(angulo));
       Serial.println(angulo);
       delay(20);
@@ -209,54 +199,107 @@ void moveServoB(int ang){
   }
 }
 
-void loop() {  
-
-  // moveServoG(150);
-
-  // moveServoD(60);
-
-  // moveServoE(90);
-
-  moveServoB(75);
-
-  moveServoG(150);
-
-  moveServoG(200);
-
-  // moveServoD(110);
-
-  // moveServoE(140);
-
-  moveServoB(155);
-
-  delay(500);
-
-  // // Varredura de 0 até 180 graus
-  // for (int angulo = 50; angulo <= 120; angulo += 10) {
-  //   // Envia o sinal PWM para os respectivos canais
-  //   pwm.setPWM(ServoGarra, 0, converterAnguloParaPulso(angulo));
-  //   pwm.setPWM(ServoDireito, 0, converterAnguloParaPulso(180 - angulo)); // Move invertido
-  //   pwm.setPWM(ServoEsquerdo, 0, converterAnguloParaPulso(angulo));
-  //   pwm.setPWM(ServoBase, 0, converterAnguloParaPulso(201));
-  //   delay(50); // Pequena pausa para dar tempo ao servo de se mover
-  // }
-
-  // delay(1000); // Aguarda 1 segundo nas posições finais
-
-  // Serial.println("Movendo servos de 180 a 0 graus...");
-  
-  // // Varredura de 180 até 0 graus
-  // for (int angulo = 120; angulo >= 50; angulo -= 10) {
-  //   pwm.setPWM(ServoGarra, 0, converterAnguloParaPulso(angulo));
-  //   pwm.setPWM(ServoDireito, 0, converterAnguloParaPulso(180 - angulo));
-  //   pwm.setPWM(ServoEsquerdo, 0, converterAnguloParaPulso(angulo));
-  //   pwm.setPWM(ServoBase, 0, converterAnguloParaPulso(180));
-  //   delay(50);
-  // }
-
-  // delay(1000); // Aguarda 1 segundo antes de reiniciar o loop
+void levanteDE(){
+  int angulod = 195;
+  for(float angulo = ultAngEsquerda; angulo <= 170; angulo += 3) {
+      pwm.setPWM(ServoEsquerdo, 0, converterAnguloParaPulso(angulo));
+      pwm.setPWM(ServoDireito, 0, converterAnguloParaPulso(angulod));
+      Serial.print("levata: ");
+      Serial.println(angulo);
+      angulod -= 3;
+      delay(20);
+      ultAngEsquerda = angulo;
+      ultAngDireito = angulod;
+    }
 }
 
+void pegarCaixa(){
+
+  moveServoB(75);
+  moveServoE(95);
+  moveServoD(185);
+  moveServoE(60);
+  moveServoD(195);
+  moveServoG(155);
+  delay(500);
+  moveServoG(205);
+  
+}
+
+void levarEsteira(){
+
+  moveServoE(70);
+  levanteDE();
+  moveServoB(155);
+  moveServoD(140);
+  moveServoE(150);
+  moveServoG(180);
+}
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Iniciando controle de servos com PCA9685 e ESP32!");
+
+  // Inicia a comunicação com o módulo
+  pwm.begin();
+  
+  // Frequência do oscilador interno do PCA9685 (geralmente 27MHz, ajusta a precisão)
+  pwm.setOscillatorFrequency(27000000);
+  
+  // Servos analógicos padrão operam a uma frequência de 50 Hz
+  pwm.setPWMFreq(50);
+
+  delay(10);
+  // pegarCaixa();
+}
+void loop() {  
+  // moveServoB(75);
+
+  pegarCaixa();
+  delay(500);
+  levarEsteira();
+  delay(500);
+
+  while(1){
+    delay(500);
+  }
+
+
+  // moveServoD(195);
+
+  // moveServoG(155);
+
+  // // moveServoD(60);
+
+  // // moveServoE(90);
+
+  // // moveServoB(75);    // caixa
+
+  // delay(500);
+  // // moveServoG(150);
+
+  // // moveServoG(200);
+
+  // // moveServoD(120);
+
+  // moveServoE(65);
+
+  // // moveServoB(155);   // esteira
+
+  // delay(500);
+  // Serial.println(ultAngEsquerda);
+}
+
+/*
+posição pré pegar caixa
+
+base = 75
+garra = 155
+direita = 195
+esquerda = 65
+*/
 
 
 
